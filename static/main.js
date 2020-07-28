f0c6a723405f60455e497bf6299b704640b84a4b
nimic <nimic@umich.edu>
2020-07-28 14:35:02 -0400

    test flickering

--- static/main.js
+++ static/main.js
@@ -1,8 +1,13 @@
+$(window).on('load', function() {
+    $('.first_load').hide();
+});
+
 $('.boutme').hover(function() {
     $('.boutme').hide('slide', {direction:'left'}, 300);
     $('.boutme2').hide('slide', {direction:'right'}, 300);
     $('.boutme_text').hide();
 });
+
 $('.boutme2').hover(function() {
     $('.boutme').hide('slide', {direction:'left'}, 300);
     $('.boutme2').hide('slide', {direction:'right'}, 300);
