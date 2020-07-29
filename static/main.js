0b57db59f39cb69cf1da6736e109cb2bc3afe1be
nimic <nimic@umich.edu>
2020-07-29 11:09:51 -0400

    changed names

--- static/main.js
+++ static/main.js
@@ -2,114 +2,114 @@ $(window).on('load', function() {
     $('.first_load').hide();
 });
 
-$('.boutme').click(function() {
-    $('#boutme_container').css('background-color', 'transparent');
-    $('.boutme').hide('slide', {direction:'left'}, 300);
-    $('.boutme2').hide('slide', {direction:'right'}, 300);
+$('.boutmediv').click(function() {
+    $('#about').css('background-color', 'transparent');
+    $('.boutmediv').hide('slide', {direction:'left'}, 300);
+    $('.boutme2div').hide('slide', {direction:'right'}, 300);
     $('.boutme_hide').hide();
 });
 
-$('.boutme2').click(function() {
-    $('#boutme_container').css('background-color', 'transparent');
-    $('.boutme').hide('slide', {direction:'left'}, 300);
-    $('.boutme2').hide('slide', {direction:'right'}, 300);
+$('.boutme2div').click(function() {
+    $('#about').css('background-color', 'transparent');
+    $('.boutmediv').hide('slide', {direction:'left'}, 300);
+    $('.boutme2div').hide('slide', {direction:'right'}, 300);
     $('.boutme_hide').hide();
 });
 
 $('.boutme_hide').click(function() {
-    $('#boutme_container').css('background-color', 'transparent');
-    $('.boutme').hide('slide', {direction:'left'}, 300);
-    $('.boutme2').hide('slide', {direction:'right'}, 300);
+    $('#about').css('background-color', 'transparent');
+    $('.boutmediv').hide('slide', {direction:'left'}, 300);
+    $('.boutme2div').hide('slide', {direction:'right'}, 300);
     $('.boutme_hide').hide();
 });
 
-$('.resume').click(function() {
-    $('#resume_container').css('background-color', 'transparent');
-    $('.resume').hide('slide', {direction:'left'}, 300);
-    $('.resume2').hide('slide', {direction:'right'}, 300);
+$('.resumediv').click(function() {
+    $('#resume').css('background-color', 'transparent');
+    $('.resumediv').hide('slide', {direction:'left'}, 300);
+    $('.resume2div').hide('slide', {direction:'right'}, 300);
     $('.resume_hide').hide();
 });
 
-$('.resume2').click(function() {
-    $('#resume_container').css('background-color', 'transparent');
-    $('.resume').hide('slide', {direction:'left'}, 300);
-    $('.resume2').hide('slide', {direction:'right'}, 300);
+$('.resume2div').click(function() {
+    $('#resume').css('background-color', 'transparent');
+    $('.resumediv').hide('slide', {direction:'left'}, 300);
+    $('.resume2div').hide('slide', {direction:'right'}, 300);
     $('.resume_hide').hide();
 });
 
 $('.resume_hide').click(function() {
-    $('#resume_container').css('background-color', 'transparent');
-    $('.resume').hide('slide', {direction:'left'}, 300);
-    $('.resume2').hide('slide', {direction:'right'}, 300);
+    $('#resume').css('background-color', 'transparent');
+    $('.resumediv').hide('slide', {direction:'left'}, 300);
+    $('.resume2div').hide('slide', {direction:'right'}, 300);
     $('.resume_hide').hide();
 });
 
-$('.interests').click(function() {
-    $('#interests_container').css('background-color', 'transparent');
-    $('.interests').hide('slide', {direction:'left'}, 300);
-    $('.interests2').hide('slide', {direction:'right'}, 300);
+$('.interestsdiv').click(function() {
+    $('#interests').css('background-color', 'transparent');
+    $('.interestsdiv').hide('slide', {direction:'left'}, 300);
+    $('.interests2div').hide('slide', {direction:'right'}, 300);
     $('.interests_hide').hide();
 });
 
-$('.interests2').click(function() {
-    $('#interests_container').css('background-color', 'transparent');
-    $('.interests').hide('slide', {direction:'left'}, 300);
-    $('.interests2').hide('slide', {direction:'right'}, 300);
+$('.interests2div').click(function() {
+    $('#interests').css('background-color', 'transparent');
+    $('.interestsdiv').hide('slide', {direction:'left'}, 300);
+    $('.interests2div').hide('slide', {direction:'right'}, 300);
     $('.interests_hide').hide();
 });
 
 $('.interests_hide').click(function() {
-    $('#interests_container').css('background-color', 'transparent');
-    $('.interests').hide('slide', {direction:'left'}, 300);
-    $('.interests2').hide('slide', {direction:'right'}, 300);
+    $('#interests').css('background-color', 'transparent');
+    $('.interestsdiv').hide('slide', {direction:'left'}, 300);
+    $('.interests2div').hide('slide', {direction:'right'}, 300);
     $('.interests_hide').hide();
 });
 
-$('.projects').click(function() {
-    $('#projects_container').css('background-color', 'transparent');
-    $('.projects').hide('slide', {direction:'left'}, 300);
-    $('.projects2').hide('slide', {direction:'right'}, 300);
+$('.projectsdiv').click(function() {
+    $('#projects').css('background-color', 'transparent');
+    $('.projectsdiv').hide('slide', {direction:'left'}, 300);
+    $('.projects2div').hide('slide', {direction:'right'}, 300);
     $('.projects_hide').hide();
 });
 
-$('.projects2').click(function() {
-    $('#projects_container').css('background-color', 'transparent');
-    $('.projects').hide('slide', {direction:'left'}, 300);
-    $('.projects2').hide('slide', {direction:'right'}, 300);
+$('.projects2div').click(function() {
+    $('#projects').css('background-color', 'transparent');
+    $('.projectsdiv').hide('slide', {direction:'left'}, 300);
+    $('.projects2div').hide('slide', {direction:'right'}, 300);
     $('.projects_hide').hide();
 });
 
 $('.projects_hide').click(function() {
-    $('#projects_container').css('background-color', 'transparent');
-    $('.projects').hide('slide', {direction:'left'}, 300);
-    $('.projects2').hide('slide', {direction:'right'}, 300);
+    $('#projects').css('background-color', 'transparent');
+    $('.projectsdiv').hide('slide', {direction:'left'}, 300);
+    $('.projects2div').hide('slide', {direction:'right'}, 300);
     $('.projects_hide').hide();
 });
 
 $('.nav-item_bout').click(function() {
-    $('#boutme_container').css('background-color', 'transparent');
-    $('.boutme').hide('slide', {direction:'left'}, 300);
-    $('.boutme2').hide('slide', {direction:'right'}, 300);
+    $('#about').css('background-color', 'transparent');
+    $('.boutmediv').hide('slide', {direction:'left'}, 300);
+    $('.boutme2div').hide('slide', {direction:'right'}, 300);
     $('.boutme_hide').hide();
 });
 
 $('.nav-item_res').click(function() {
-    $('#resume_container').css('background-color', 'transparent');
-    $('.resume').hide('slide', {direction:'left'}, 300);
-    $('.resume2').hide('slide', {direction:'right'}, 300);
+    $('#resume').css('background-color', 'transparent');
+    $('.resumediv').hide('slide', {direction:'left'}, 300);
+    $('.resume2div').hide('slide', {direction:'right'}, 300);
     $('.resume_hide').hide();
 });
 
 $('.nav-item_inter').click(function() {
-    $('#interests_container').css('background-color', 'transparent');
-    $('.interests').hide('slide', {direction:'left'}, 300);
-    $('.interests2').hide('slide', {direction:'right'}, 300);
+    $('#interests').css('background-color', 'transparent');
+    $('.interestsdiv').hide('slide', {direction:'left'}, 300);
+    $('.interests2div').hide('slide', {direction:'right'}, 300);
     $('.interests_hide').hide();
 });
 
 $('.nav-item_pro').click(function() {
-    $('#projects_container').css('background-color', 'transparent');
-    $('.projects').hide('slide', {direction:'left'}, 300);
-    $('.projects2').hide('slide', {direction:'right'}, 300);
+    $('#projects').css('background-color', 'transparent');
+    $('.projectsdiv').hide('slide', {direction:'left'}, 300);
+    $('.projects2div').hide('slide', {direction:'right'}, 300);
     $('.projects_hide').hide();
 })
