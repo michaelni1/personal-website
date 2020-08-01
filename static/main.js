c7f9c10d452d12bdafc4cd4843a00a6ec1aaa117
nimic <nimic@umich.edu>
2020-07-31 22:40:07 -0400

    changed transit speed

--- static/main.js
+++ static/main.js
@@ -25,128 +25,128 @@ function display_space() {
 
 $('.boutme_transit').click(function() {
     $('#about_div').css('background-color', 'transparent');
-    $('.boutme_transit').hide('slide', {direction:'left'}, 5000);
-    $('.boutme2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.boutme_transit').hide('slide', {direction:'left'}, 450);
+    $('.boutme2_transit').hide('slide', {direction:'right'}, 450);
     $('.boutme_hide').hide();
     $('#about_div').css('height', '');
 });
 
 $('.boutme2_transit').click(function() {
     $('#about_div').css('background-color', 'transparent');
-    $('.boutme_transit').hide('slide', {direction:'left'}, 5000);
-    $('.boutme2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.boutme_transit').hide('slide', {direction:'left'}, 450);
+    $('.boutme2_transit').hide('slide', {direction:'right'}, 450);
     $('.boutme_hide').hide();
     $('#about_div').css('height', '');
 });
 
 $('.boutme_hide').click(function() {
     $('#about_div').css('background-color', 'transparent');
-    $('.boutme_transit').hide('slide', {direction:'left'}, 5000);
-    $('.boutme2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.boutme_transit').hide('slide', {direction:'left'}, 450);
+    $('.boutme2_transit').hide('slide', {direction:'right'}, 450);
     $('.boutme_hide').hide();
     $('#about_div').css('height', '');
 });
 
 $('.resume_transit').click(function() {
     $('#resume_div').css('background-color', 'transparent');
-    $('.resume_transit').hide('slide', {direction:'left'}, 5000);
-    $('.resume2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.resume_transit').hide('slide', {direction:'left'}, 450);
+    $('.resume2_transit').hide('slide', {direction:'right'}, 450);
     $('.resume_hide').hide();
     $('#resume_div').css('height', '');
 });
 
 $('.resume2_transit').click(function() {
     $('#resume_div').css('background-color', 'transparent');
-    $('.resume_transit').hide('slide', {direction:'left'}, 5000);
-    $('.resume2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.resume_transit').hide('slide', {direction:'left'}, 450);
+    $('.resume2_transit').hide('slide', {direction:'right'}, 450);
     $('.resume_hide').hide();
     $('#resume_div').css('height', '');
 });
 
 $('.resume_hide').click(function() {
     $('#resume_div').css('background-color', 'transparent');
-    $('.resume_transit').hide('slide', {direction:'left'}, 5000);
-    $('.resume2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.resume_transit').hide('slide', {direction:'left'}, 450);
+    $('.resume2_transit').hide('slide', {direction:'right'}, 450);
     $('.resume_hide').hide();
     $('#resume_div').css('height', '');
 });
 
 $('.interests_transit').click(function() {
     $('#interests_div').css('background-color', 'transparent');
-    $('.interests_transit').hide('slide', {direction:'left'}, 5000);
-    $('.interests2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.interests_transit').hide('slide', {direction:'left'}, 450);
+    $('.interests2_transit').hide('slide', {direction:'right'}, 450);
     $('.interests_hide').hide();
     $('#interests_div').css('height', '');
 });
 
 $('.interests2_transit').click(function() {
     $('#interests_div').css('background-color', 'transparent');
-    $('.interests_transit').hide('slide', {direction:'left'}, 5000);
-    $('.interests2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.interests_transit').hide('slide', {direction:'left'}, 450);
+    $('.interests2_transit').hide('slide', {direction:'right'}, 450);
     $('.interests_hide').hide();
     $('#interests_div').css('height', '');
 });
 
 $('.interests_hide').click(function() {
     $('#interests_div').css('background-color', 'transparent');
-    $('.interests_transit').hide('slide', {direction:'left'}, 5000);
-    $('.interests2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.interests_transit').hide('slide', {direction:'left'}, 450);
+    $('.interests2_transit').hide('slide', {direction:'right'}, 450);
     $('.interests_hide').hide();
     $('#interests_div').css('height', '');
 });
 
 $('.projects_transit').click(function() {
     $('#projects_div').css('background-color', 'transparent');
-    $('.projects_transit').hide('slide', {direction:'left'}, 5000);
-    $('.projects2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.projects_transit').hide('slide', {direction:'left'}, 450);
+    $('.projects2_transit').hide('slide', {direction:'right'}, 450);
     $('.projects_hide').hide();
     $('#projects_div').css('height', '');
 });
 
 $('.projects2_transit').click(function() {
     $('#projects_div').css('background-color', 'transparent');
-    $('.projects_transit').hide('slide', {direction:'left'}, 5000);
-    $('.projects2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.projects_transit').hide('slide', {direction:'left'}, 450);
+    $('.projects2_transit').hide('slide', {direction:'right'}, 450);
     $('.projects_hide').hide();
     $('#projects_div').css('height', '');
 });
 
 $('.projects_hide').click(function() {
     $('#projects_div').css('background-color', 'transparent');
-    $('.projects_transit').hide('slide', {direction:'left'}, 5000);
-    $('.projects2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.projects_transit').hide('slide', {direction:'left'}, 450);
+    $('.projects2_transit').hide('slide', {direction:'right'}, 450);
     $('.projects_hide').hide();
     $('#projects_div').css('height', '');
 });
 
 $('.nav-item_bout').click(function() {
     $('#about_div').css('background-color', 'transparent');
-    $('.boutme_transit').hide('slide', {direction:'left'}, 5000);
-    $('.boutme2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.boutme_transit').hide('slide', {direction:'left'}, 450);
+    $('.boutme2_transit').hide('slide', {direction:'right'}, 450);
     $('.boutme_hide').hide();
     $('#about_div').css('height', '');
 });
 
 $('.nav-item_res').click(function() {
     $('#resume_div').css('background-color', 'transparent');
-    $('.resume_transit').hide('slide', {direction:'left'}, 5000);
-    $('.resume2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.resume_transit').hide('slide', {direction:'left'}, 450);
+    $('.resume2_transit').hide('slide', {direction:'right'}, 450);
     $('.resume_hide').hide();
     $('#resume_div').css('height', '');
 });
 
 $('.nav-item_inter').click(function() {
     $('#interests_div').css('background-color', 'transparent');
-    $('.interests_transit').hide('slide', {direction:'left'}, 5000);
-    $('.interests2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.interests_transit').hide('slide', {direction:'left'}, 450);
+    $('.interests2_transit').hide('slide', {direction:'right'}, 450);
     $('.interests_hide').hide();
     $('#interests_div').css('height', '');
 });
 
 $('.nav-item_pro').click(function() {
     $('#projects_div').css('background-color', 'transparent');
-    $('.projects_transit').hide('slide', {direction:'left'}, 5000);
-    $('.projects2_transit').hide('slide', {direction:'right'}, 5000);
+    $('.projects_transit').hide('slide', {direction:'left'}, 450);
+    $('.projects2_transit').hide('slide', {direction:'right'}, 450);
     $('.projects_hide').hide();
     $('#projects_div').css('height', '');
 })
